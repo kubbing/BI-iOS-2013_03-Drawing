2e220668bbba89e4a037dc632809bc0e65308235
Jakub Hladik <kubbing@me.com>
2013-10-07 07:56:57 +0200

    Something fixed (courtesy of http://whatthecommit.com/)

---
+++ BI-iOS-2013_03-Drawing/ChartView.h
@@ -0,0 +1,13 @@
+//
+//  ChartView.h
+//  BI-iOS-2013_03-Drawing
+//
+//  Created by Jakub Hladík on 07.10.13.
+//  Copyright (c) 2013 FIT CTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChartView : UIView
+
+@end
--- BI-iOS-2013_03-Drawing/FirstViewController.m
+++ BI-iOS-2013_03-Drawing/FirstViewController.m
@@ -8,7 +8,13 @@
 
 #import "FirstViewController.h"
 
+#include "ChartView.h"
+#include "PanelView.h"
+
 @interface FirstViewController ()
+
+@property (weak, nonatomic) ChartView *chartView;
+@property (weak, nonatomic) PanelView *panelView;
 
 @end
 
@@ -18,6 +24,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    ChartView *chartView = [[ChartView alloc] initWithFrame:CGRectZero];
+    chartView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [self.view addSubview:chartView];
+    self.chartView = chartView;
+    
+    PanelView *panelView = [[PanelView alloc] initWithFrame:CGRectZero];
+    panelView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [self.view addSubview:panelView];
+    self.panelView = panelView;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    self.chartView.frame = CGRectMake(8, 20 + 8, CGRectGetWidth(self.view.bounds) - 16, 512);
+    self.panelView.frame = CGRectMake(8, 20 + 512 + 16, CGRectGetWidth(self.view.bounds) - 16, 128);
 }
 
 - (void)didReceiveMemoryWarning
